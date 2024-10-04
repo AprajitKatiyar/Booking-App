7bac458039c182691a0bd5ece2fbf414ce449028
Aprajit Katiyar <aprajit.katiyar1999@gmail.com>
2024-10-05 03:26:13 +0530

    Looping over the bookings

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
@@ -18,39 +21,46 @@ func main() {
 	//OR
 	//bookings:=[]string{}
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	//Infinite loop to keep taking user input
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	//take user input
-	fmt.Println("Please enter your first name: ")
-	fmt.Scan(&firstName)
+		//take user input
+		fmt.Println("Please enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Please enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Please enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Println("How many tickets would you like to book? ")
-	fmt.Scan(&userTickets)
+		fmt.Println("How many tickets would you like to book? ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+""+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole slice is %v\n", bookings)
-	fmt.Printf("The first element is %v\n", bookings[0])
-	fmt.Printf("Type of the slice is %T\n", bookings)
-	fmt.Printf("The length of the slice is %v\n", len(bookings))
+		//To print the memory address of the variable
+		//fmt.Println(&remainingTickets)
 
-	//To print the memory address of the variable
-	//fmt.Println(&remainingTickets)
+		fmt.Printf("Hello %v %v, you have booked %v tickets for the conference.\n", firstName, lastName, userTickets)
+		fmt.Printf("You will receive a confirmation email at %v\n", email)
 
-	fmt.Printf("Hello %v %v, you have booked %v tickets for the conference.\n", firstName, lastName, userTickets)
-	fmt.Printf("You will receive a confirmation email at %v\n", email)
+		fmt.Printf("Remaining tickets: %v\n", remainingTickets)
 
-	fmt.Printf("Remaining tickets: %v", remainingTickets)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			var firstName = names[0]
+			firstNames = append(firstNames, firstName)
+		}
+
+		fmt.Printf("The first names of the bookings are %v\n", firstNames)
+	}
 
 	//Print the data type of the variables
 	//fmt.Printf("Data type of conferenceName is %T\n", conferenceName)
